6f280bde4d61fec16883fc05406293d2a201b31b
dominikdobos <dominik.dobos33@gmail.com>
2024-04-04 11:13:50 +0200

    index js meghívás és javítás

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { palyaLetrehoz, palyaMegjelenit } from "./dd.js";
-import { lekapcsoltSzamlalo, kiiratas } from "./bo.js";
+import { lekapcsoltSzamlalo, kiiratas } from "./bs.js";
 
 function jatekLetrehoz() {
   const LISTA = palyaLetrehoz();
   palyaMegjelenit(LISTA);
   kiiratas(lekapcsoltSzamlalo(LISTA));
 }
+jatekLetrehoz();
